d6200731c81961c5b61a4cba407a7c7e985450a0
jlcmar <145810805+jlcmar@users.noreply.github.com>
2023-09-22 17:15:55 -0500

    Update test-file1.js

--- test-file1.js
+++ test-file1.js
@@ -1,4 +1,4 @@
-define(['pipAPI', 'https://github.com/jlcmar/IAT/blob/main/test-file2.js'], function(APIConstructor, iatExtension){
+define(['pipAPI', 'https://jlcmar.github.io/IAT/test-file2.js'], function(APIConstructor, iatExtension){
     var API = new APIConstructor();
 
 	return iatExtension({
