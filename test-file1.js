527aa65cdb521d4afa57a93ef0be28b245a336bd
jlcmar <145810805+jlcmar@users.noreply.github.com>
2023-10-24 10:08:02 -0500

    Update test-file1.js

--- test-file1.js
+++ test-file1.js
@@ -113,6 +113,151 @@ define(['pipAPI', 'https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/qualt
 		
 		finalText : 'Press space to exit the test', 
 		finalTouchText : 'Touch the bottom green area to exit the test',
+
+		touchMaxStimulusWidth : '50%', 
+		touchMaxStimulusHeight : '50%', 
+		bottomTouchCss: {}, //Add any CSS value you want for changing the css of the bottom touch area.
+
+		//Instructions text.
+		// You can use the following variables and they will be replaced by
+		// the name of the categories and the block's number variables:
+		// leftCategory, rightCategory, leftAttribute and rightAttribute, blockNum, nBlocks.
+		// Notice that this is HTML text.
+		instAttributePractice: '<div><p align="center" style="font-size:20px; font-family:arial">' +
+			'<font color="#000000"><u>Part blockNum of nBlocks </u><br/><br/></p>' +
+			'<p style="font-size:20px; text-align:left; vertical-align:bottom; margin-left:10px; font-family:arial">' +
+			'Put a left finger on the <b>E</b> key for items that belong to the category <font color="#0000ff">leftAttribute.</font>' +
+			'<br/>Put a right finger on the <b>I</b> key for items that belong to the category <font color="#0000ff">rightAttribute</font>.<br/><br/>' +
+			'If you make a mistake, a red <font color="#ff0000"><b>X</b></font> will appear. ' +
+			'Press the other key to continue.<br/>' +
+			'<u>Go as fast as you can</u> while being accurate.<br/><br/></p>'+
+			'<p align="center">Press the <b>space bar</b> when you are ready to start.</font></p></div>',
+		instAttributePracticeTouch: [
+			'<div>',
+				'<p align="center">',
+					'<u>Part blockNum of nBlocks</u>',
+				'</p>',
+				'<p align="left" style="margin-left:5px">',
+					'<br/>',
+					'Put a left finger over the the <b>left</b> green area for items that belong to the category <font color="#0000ff">leftAttribute</font>.<br/>',
+					'Put a right finger over the <b>right</b> green area for items that belong to the category <font color="#0000ff">rightAttribute</font>.<br/>',
+					'Items will appear one at a time.<br/>',
+					'<br/>',
+					'If you make a mistake, a red <font color="#ff0000"><b>X</b></font> will appear. Touch the other side. <u>Go as fast as you can</u> while being accurate.',
+				'</p>',
+				'<p align="center">Touch the <b>lower </b> green area to start.</p>',
+			'</div>'
+		].join('\n'),
+
+		instCategoriesPractice: '<div><p align="center" style="font-size:20px; font-family:arial">' +
+			'<font color="#000000"><u>Part blockNum of nBlocks </u><br/><br/></p>' +
+			'<p style="font-size:20px; text-align:left; vertical-align:bottom; margin-left:10px; font-family:arial">' +
+			'Put a left finger on the <b>E</b> key for items that belong to the category <font color="#31940F">leftCategory</font>. ' +
+			'<br/>Put a right finger on the <b>I</b> key for items that belong to the category <font color="#31940F">rightCategory</font>.<br/>' +
+			'Items will appear one at a time.<br/><br/>' +
+			'If you make a mistake, a red <font color="#ff0000"><b>X</b></font> will appear. ' +
+			'Press the other key to continue.<br/>' +
+			'<u>Go as fast as you can</u> while being accurate.<br/><br/></p>'+
+			'<p align="center">Press the <b>space bar</b> when you are ready to start.</font></p></div>',
+		instCategoriesPracticeTouch: [
+			'<div>',
+				'<p align="center">',
+					'<u>Part blockNum of nBlocks</u>',
+				'</p>',
+				'<p align="left" style="margin-left:5px">',
+					'<br/>',
+					'Put a left finger over the <b>left</b> green area for items that belong to the category <font color="#31940F">leftCategory</font>.<br/>',
+					'Put a right finger over the <b>right</b> green area for items that belong to the category <font color="#31940F">rightCategory</font>.<br/>',
+					'Items will appear one at a time.<br/>',
+					'<br/>',
+					'If you make a mistake, a red <font color="#ff0000"><b>X</b></font> will appear. Touch the other side. <u>Go as fast as you can</u> while being accurate.',
+				'</p>',
+				'<p align="center">Touch the <b>lower </b> green area to start.</p>',
+			'</div>'
+		].join('\n'),
+
+		instFirstCombined : '<div><p align="center" style="font-size:20px; font-family:arial">' +
+			'<font color="#000000"><u>Part blockNum of nBlocks </u><br/><br/></p>' +
+			'<p style="font-size:20px; text-align:left; vertical-align:bottom; margin-left:10px; font-family:arial">' +
+			'Use the <b>E</b> key for <font color="#31940F">leftCategory</font> and for <font color="#0000ff">leftAttribute</font>.<br/>' +
+			'Use the <b>I</b> key for <font color="#31940F">rightCategory</font> and for  <font color="#0000ff">rightAttribute</font>.<br/>' +
+			'Each item belongs to only one category.<br/><br/>' +
+			'If you make a mistake, a red <font color="#ff0000"><b>X</b></font> will appear. ' +
+			'Press the other key to continue.<br/>' + 
+			'<u>Go as fast as you can</u> while being accurate.<br/><br/></p>' +
+			'<p align="center">Press the <b>space bar</b> when you are ready to start.</font></p></div>',
+		instFirstCombinedTouch:[
+			'<div>',
+				'<p align="center">',
+					'<u>Part blockNum of nBlocks</u>',
+				'</p>',
+				'<br/>',
+				'<br/>',
+				'<p align="left" style="margin-left:5px">',
+					'Put a left finger over the <b>left</b> green area for <font color="#31940F">leftCategory</font> items and for <font color="#0000ff">leftAttribute</font>.</br>',
+					'Put a right finger over the <b>right</b> green area for <font color="#31940F">rightCategory</font> items and for <font color="#0000ff">rightAttribute</font>.</br>',
+						'If you make a mistake, a red <font color="#ff0000"><b>X</b></font> will appear. Touch the other side. <u>Go as fast as you can</u> while being accurate.</br>',
+					'</p>',
+					'<p align="center">Touch the <b>lower </b> green area to start.</p>',
+			'</div>'
+		].join('\n'),
+
+		instSecondCombined : '<div><p align="center" style="font-size:20px; font-family:arial">' +
+			'<font color="#000000"><u>Part blockNum of nBlocks </u><br/><br/></p>' +
+			'<p style="font-size:20px; text-align:left; vertical-align:bottom; margin-left:10px; font-family:arial">' +
+			'This is the same as the previous part.<br/>' +
+			'Use the <b>E</b> key for <font color="#31940F">leftCategory</font> and for <font color="#0000ff">leftAttribute</font>.<br/>' +
+			'Use the <b>I</b> key for <font color="#31940F">rightCategory</font> and for  <font color="#0000ff">rightAttribute</font>.<br/>' +
+			'Each item belongs to only one category.<br/><br/>' +
+			'<u>Go as fast as you can</u> while being accurate.<br/><br/></p>' +
+			'<p align="center">Press the <b>space bar</b> when you are ready to start.</font></p></div>',
+		instSecondCombinedTouch:[
+			'<div>',
+				'<p align="center"><u>Part blockNum of nBlocks</u></p>',
+				'<br/>',
+				'<br/>',
+
+				'<p align="left" style="margin-left:5px">',
+					'Put a left finger over the <b>left</b> green area for <font color="#31940F">leftCategory</font> items and for <font color="#0000ff">leftAttribute</font>.<br/>',
+					'Put a right finger over the <b>right</b> green area for <font color="#31940F">rightCategory</font> items and for <font color="#0000ff">rightAttribute</font>.<br/>',
+					'<br/>',
+					'<u>Go as fast as you can</u> while being accurate.<br/>',
+				'</p>',
+				'<p align="center">Touch the <b>lower </b> green area to start.</p>',
+			'</div>'
+		].join('\n'),
+
+		instSwitchCategories : '<div><p align="center" style="font-size:20px; font-family:arial">' +
+			'<font color="#000000"><u>Part blockNum of nBlocks </u><br/><br/></p>' +
+			'<p style="font-size:20px; text-align:left; vertical-align:bottom; margin-left:10px; font-family:arial">' +
+			'<b>Watch out, the labels have changed position!</b><br/>' +
+			'Put the left finger on the <b>E</b> key for <font color="#31940F">leftCategory</font>.<br/>' +
+			'Put the right finger on the <b>I</b> key for <font color="#31940F">rightCategory</font>.<br/><br/>' +
+			'<u>Go as fast as you can</u> while being accurate.<br/><br/></p>' +
+			'<p align="center">Press the <b>space bar</b> when you are ready to start.</font></p></div>',
+		instSwitchCategoriesTouch: [
+			'<div>',
+				'<p align="center">',
+					'<u>Part blockNum of nBlocks</u>',
+				'</p>',
+				'<p align="left" style="margin-left:5px">',
+					'<br/>',
+					'Watch out, the labels have changed position!<br/>',
+						'Put a left finger over the <b>left</b> green area for <font color="#31940F">leftCategory</font> items.<br/>',
+						'Put a right finger over the <b>right</b> green area for <font color="#31940F">rightCategory</font> items.<br/>',
+						'Items will appear one at a time.',
+						'<br/>',
+						'If you make a mistake, a red <font color="#ff0000"><b>X</b></font> will appear. Touch the other side. <u>Go as fast as you can</u> while being accurate.<br/>',
+					'</p>',
+					'<p align="center">Touch the <b>lower </b> green area to start.</p>',
+			'</div>'
+		].join('\n'),
+
+		instThirdCombined : 'instFirstCombined', //this means that we're going to use the instFirstCombined property for the third combined block as well. You can change that.
+		instFourthCombined : 'instSecondCombined', //this means that we're going to use the instSecondCombined property for the fourth combined block as well. You can change that.
+		instThirdCombinedTouch : 'instFirstCombined', //this means that we're going to use the instFirstCombined property for the third combined block as well. You can change that.
+		instFourthCombinedTouch : 'instSecondCombined', //this means that we're going to use the instSecondCombined property for the fourth combined block as well. You can change that.
+		
 		showDebriefing:true, //Show feedback in the last trial? Relevant only in a Qualtrics IAT because in Qualtrics we cannot access the saved feedback and IAT score later in the survey.
 		//Texts for the trials that show the debriefing.
 		preDebriefingText : 'Press space to see your result', //Text in the trial that comes before showing the debriefing.
